b6cf72676242631d2e7814ac216339c59265e945
kbvernon <kbvernon@users.noreply.github.com>
2025-05-03 14:26:08 +0000

    deploy: e0ff1ea757b0b2dcc61771c01fc5f9996e315dbb

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
-window.ALL_CRATES = ["extendr_api","extendr_engine","extendr_macros","xtask"];
+window.ALL_CRATES = ["extendr_api","extendr_engine","extendr_ffi","extendr_macros","xtask"];
+//{"start":21,"fragment_lengths":[13,17,14,17,8]}
